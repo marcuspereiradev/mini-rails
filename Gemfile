88100b42df962151b23ce6f36c7cebf571bd00fb
Marcus Pereira <marcus.work@hotmail.com>
2020-11-12 15:48:19 -0300

    Setting up mini-rails

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gemspec path: '../..'
+
+gem "rake", "~> 12.0"
---
+++ test/muffin_blog/db/seeds.rb
@@ -0,0 +1,19 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Post.create(
+  title: 'Blueberry Muffins',
+  body: 'The incentive to technological advancement, as well as the increase in dialogue 
+    between the different productive sectors, guarantees the contribution of an important 
+    group in determining all the functional resources involved.'
+  )
+Post.create(
+  title: 'Chocolate Chip Muffins', 
+  body: 'We can already see the way in which the mobility of international capital 
+    guarantees the contribution of an important group in determining 
+    the communication process as a whole.'
+  )
